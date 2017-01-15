5572d29a03c71f417067819e3924fc170d468406
martoh <marty.lyn@gmail.com>
2017-01-15 17:01:07 -0500

    Basic work-flow setup. Intents created to pass between Activities

--- app/src/main/java/akitasoft/bydbuzz/com/bydbuzz/LoginActivity.java
+++ app/src/main/java/akitasoft/bydbuzz/com/bydbuzz/LoginActivity.java
@@ -1,7 +1,10 @@
 package akitasoft.bydbuzz.com.bydbuzz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button button = (Button) findViewById(R.id.btn_sign_in);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), VenueActivity.class);
+//                intent.putExtra("sampleText", "thereisnocowlevel");
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/akitasoft/bydbuzz/com/bydbuzz/VenueActivity.java
+++ app/src/main/java/akitasoft/bydbuzz/com/bydbuzz/VenueActivity.java
@@ -1,7 +1,10 @@
 package akitasoft.bydbuzz.com.bydbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VenueActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class VenueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue);
+
+        final Button button = (Button) findViewById(R.id.btn_upgrade);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SeatActivity.class);
+//                intent.putExtra("sampleText", "thereisnocowlevel");
+                startActivity(intent);
+            }
+        });
     }
 }
